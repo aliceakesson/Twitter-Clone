0d1f7f78da87c92f9a0bc868e10b3716c17bf7bf
aliceakesson <alicek732@gmail.com>
2023-08-24 12:30:26 +0200

    Added elements for a tweet

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const textareaPost = document.getElementById('textareaPost');
 
 const lightGrey = getComputedStyle(document.documentElement).getPropertyValue('--light-grey')
 
+posts = []
+
 // setInterval(run, 1000);
 
 function run() {
     if(textareaPost.value.length > 0) {
         textareaPost.style.color = lightGrey;
     }
+}
+
+newPost('alice', 'This is a test post.');
+
+function newPost(username, context) {
+    
 }
 
