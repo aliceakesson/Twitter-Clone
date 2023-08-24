0447c87b1b29ad7b3f1e16b097f9fef5f018656d
aliceakesson <alicek732@gmail.com>
2023-08-24 13:30:59 +0200

    Like function done

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ var profiles = {'alice': 'profile.png'};
 const tweet = document.getElementById('tweet-replica');
 
 const lightGrey = getComputedStyle(document.documentElement).getPropertyValue('--light-grey')
-
-posts = []
+const pink = getComputedStyle(document.documentElement).getPropertyValue('--pink');
 
 // setInterval(run, 1000);
 
@@ -30,5 +29,30 @@ function newPost(username, context) {
     clone.querySelector('.tweet-name').innerHTML = names[username];
     clone.querySelector('.tweet-username').innerHTML = "@" + username;
     clone.querySelector('.tweet-post').innerHTML = context;
-}
 
+    const likeDiv = clone.querySelector('.tweet-like');
+    likeDiv.addEventListener('click', e => {
+        const heart = likeDiv.querySelector('i');
+        if(heart.classList.contains('fa-regular')) {
+            heart.classList.remove('fa-regular');
+            heart.classList.add('fa-solid');
+            likeDiv.style.color = pink; 
+
+            const likeP = likeDiv.querySelector('p');
+            const amountOfLikes = parseInt(likeP.innerHTML);
+            if(amountOfLikes == 0) 
+                likeP.style.visibility = 'visible';
+            likeP.innerHTML = amountOfLikes + 1;
+        } else {
+            heart.classList.remove('fa-solid');
+            heart.classList.add('fa-regular');
+            likeDiv.style.color = lightGrey;
+
+            const likeP = likeDiv.querySelector('p');
+            const amountOfLikes = parseInt(likeP.innerHTML);
+            if(amountOfLikes == 1) 
+                likeP.style.visibility = 'hidden';
+            likeP.innerHTML = amountOfLikes - 1;
+        }
+    });
+}
